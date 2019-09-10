478cfb99fe6752e51112e856f18c9d20f04ac415
坂本将紀 <masaki19821208@gmail.com>
2019-09-11 07:53:13 +0900

    第４回　画面遷移・スプラッシュ（実装完了）

--- ITPMApp/app/src/main/java/com/example/itpmapp/activities/SplashActivity.java
+++ ITPMApp/app/src/main/java/com/example/itpmapp/activities/SplashActivity.java
@@ -2,15 +2,38 @@ package com.example.itpmapp.activities;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
 
 import com.example.itpmapp.R;
 
 public class SplashActivity extends AppCompatActivity {
 
+    Handler mHandler = new Handler();
+    Runnable mRunnable = new Runnable() {
+        @Override
+        public void run() {
+            Intent intent = new Intent(SplashActivity.this, MainActivity.class);
+            startActivity(intent);
+            finish();
+        }
+    };
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash);
+
+        mHandler.postDelayed(mRunnable, 2000);
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        if (mHandler != null) {
+            mHandler.removeCallbacks(mRunnable);
+            mHandler = null;
+        }
     }
 }
