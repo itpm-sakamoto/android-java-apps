8859456643431ce832b36718a5f7785ce017b547
坂本将紀 <masaki19821208@gmail.com>
2019-09-14 13:11:31 +0900

    第５回　リスト表示（前編）（実装完了）

--- ITPMApp/app/src/main/java/com/example/itpmapp/activities/MainActivity.java
+++ ITPMApp/app/src/main/java/com/example/itpmapp/activities/MainActivity.java
@@ -11,8 +11,19 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 
 import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.Toast;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
+
+    private ListView mListView;
+    private ArrayAdapter<String> mAdapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,6 +31,30 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+
+        mListView = findViewById(R.id.main_list);
+
+        mAdapter = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1);
+
+        mListView.setAdapter(mAdapter);
+        mListView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> adapterView, View view, int position, long l) {
+                String title = String.valueOf(adapterView.getItemAtPosition(position));
+                Intent intent = EditActivity.createIntent(MainActivity.this, title);
+                startActivity(intent);
+            }
+        });
+
+        mListView.setOnItemLongClickListener(new AdapterView.OnItemLongClickListener() {
+            @Override
+            public boolean onItemLongClick(AdapterView<?> adapterView, View view, int position, long l) {
+                String title = String.valueOf(adapterView.getItemAtPosition(position));
+                mAdapter.remove(title);
+                Toast.makeText(MainActivity.this, title + "を削除しました。", Toast.LENGTH_SHORT).show();
+                return true;
+            }
+        });
 
         FloatingActionButton fab = findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
@@ -29,5 +64,21 @@ public class MainActivity extends AppCompatActivity {
                 startActivity(intent);
             }
         });
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        displayDataList();
+    }
+
+    /**
+     * リスト表示用メソッド
+     */
+    private void displayDataList() {
+        mAdapter.clear();
+        List<String> dataList = Arrays.asList("ホーム", "事業内容", "企業情報", "採用情報", "お問い合わせ");
+        mAdapter.addAll(dataList);
+        mAdapter.notifyDataSetChanged();
     }
 }
