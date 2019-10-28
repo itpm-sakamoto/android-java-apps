d17f80867daea09e6795fa1e4d143396deddc19b
坂本将紀 <masaki19821208@gmail.com>
2019-10-29 08:29:29 +0900

    第６回　リスト表示（後編）（実装完了）

--- ITPMApp/app/src/main/java/com/example/itpmapp/activities/MainActivity.java
+++ ITPMApp/app/src/main/java/com/example/itpmapp/activities/MainActivity.java
@@ -1,19 +1,26 @@
 package com.example.itpmapp.activities;
 
+import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
 
 import com.example.itpmapp.R;
+import com.example.itpmapp.pojo.TitleDataItem;
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
 import com.google.android.material.snackbar.Snackbar;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 
+import android.view.LayoutInflater;
 import android.view.View;
+import android.view.ViewGroup;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import java.util.ArrayList;
@@ -23,7 +30,8 @@ import java.util.List;
 public class MainActivity extends AppCompatActivity {
 
     private ListView mListView;
-    private ArrayAdapter<String> mAdapter;
+//    private ArrayAdapter<String> mAdapter;
+    private MainListAdapter mAdapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -35,7 +43,8 @@ public class MainActivity extends AppCompatActivity {
         mListView = findViewById(R.id.main_list);
 
         // 2.Adapterを作成する。
-        mAdapter = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, new ArrayList<String>());
+//        mAdapter = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, new ArrayList<String>());
+        mAdapter = new MainListAdapter(this, R.layout.layout_title_item, new ArrayList<TitleDataItem>());
 
         // 3.ListViewにAdapterをセットする。
         mListView.setAdapter(mAdapter);
@@ -44,8 +53,10 @@ public class MainActivity extends AppCompatActivity {
         mListView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
             @Override
             public void onItemClick(AdapterView<?> adapterView, View view, int position, long l) {
-                String title = String.valueOf(adapterView.getItemAtPosition(position));
-                Intent intent = EditActivity.createIntent(MainActivity.this, title);
+                TitleDataItem item = (TitleDataItem)adapterView.getItemAtPosition(position);
+                Intent intent = EditActivity.createIntent(MainActivity.this, item.getTitle());
+//                String title = String.valueOf(adapterView.getItemAtPosition(position));
+//                Intent intent = EditActivity.createIntent(MainActivity.this, title);
                 startActivity(intent);
             }
         });
@@ -54,10 +65,13 @@ public class MainActivity extends AppCompatActivity {
         mListView.setOnItemLongClickListener(new AdapterView.OnItemLongClickListener() {
             @Override
             public boolean onItemLongClick(AdapterView<?> adapterView, View view, int position, long l) {
-                String title = String.valueOf(adapterView.getItemAtPosition(position));
-                mAdapter.remove(title);
-                Toast.makeText(MainActivity.this, title + "を削除しました。", Toast.LENGTH_SHORT).show();
+                TitleDataItem item = (TitleDataItem)adapterView.getItemAtPosition(position);
+                mAdapter.remove(item);
+//                String title = String.valueOf(adapterView.getItemAtPosition(position));
+//                mAdapter.remove(title);
+                Toast.makeText(MainActivity.this, item.getTitle() + "を削除しました。", Toast.LENGTH_SHORT).show();
                 return true;
+//                return false;
             }
         });
 
@@ -85,10 +99,59 @@ public class MainActivity extends AppCompatActivity {
         mAdapter.clear();
 
         // 2.新しいデータをアダプターに設定する（セットする）
-        List<String> dataList = Arrays.asList("ホーム", "事業内容", "企業情報", "採用情報", "お問い合わせ");
+//        List<String> dataList = Arrays.asList("ホーム", "事業内容", "企業情報", "採用情報", "お問い合わせ");
+        List<TitleDataItem> dataList = Arrays.asList(
+                new TitleDataItem(1, "ホーム"),
+                new TitleDataItem(2, "事業内容"),
+                new TitleDataItem(3, "企業情報"),
+                new TitleDataItem(4, "採用情報"),
+                new TitleDataItem(5, "お問い合わせ")
+        );
         mAdapter.addAll(dataList);
 
         // 3.アダプターにデータが変更されたことを教えてあげる（通知する）
         mAdapter.notifyDataSetChanged();
+    }
+
+    private class MainListAdapter extends ArrayAdapter<TitleDataItem> {
+
+        private LayoutInflater layoutInflater;
+        private int resource;
+        private List<TitleDataItem> dataList;
+
+        public MainListAdapter(@NonNull Context context, int resource, @NonNull List<TitleDataItem> objects) {
+            super(context, resource, objects);
+            this.layoutInflater = LayoutInflater.from(context);
+            this.resource = resource;
+            this.dataList = objects;
+        }
+
+        @Override
+        public int getCount() {
+            return dataList.size();
+        }
+
+        @Nullable
+        @Override
+        public TitleDataItem getItem(int position) {
+            return dataList.get(position);
+        }
+
+        @NonNull
+        @Override
+        public View getView(int position, @Nullable View convertView, @NonNull ViewGroup parent) {
+            TitleDataItem item = dataList.get(position);
+            TextView titleTextView;
+            if (convertView == null) {
+                convertView = layoutInflater.inflate(resource, null);
+                titleTextView = convertView.findViewById(R.id.title_text_view);
+                convertView.setTag(titleTextView);
+            } else {
+                titleTextView = (TextView) convertView.getTag();
+            }
+            titleTextView.setText(item.getTitle());
+
+            return convertView;
+        }
     }
 }
